25fe2c30c59459440e70449c1731245fadffce8a
jaiboy825 <55534787+jaiboy825@users.noreply.github.com>
2021-06-17 17:17:45 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,56 @@
 window.addEventListener("load", () => {
-    $.getJSON("/data.json", (data) => {
-        let app = new App(data);
-    })
+    // $.getJSON("/data.json", (data) => {
+        // let app = new App(data);
+    // })
+    let app = new App();
 });
 
 class App {
-    constructor(text) {
-        this.text = text;
+    constructor() {
+        this.text = [{
+            "text" : "삶이 있는 한 희망은 있다.",
+            "writer" : "키케로"
+        },
+        {
+            "text" : "산다는 것 그것은 치열한 전투이다.",
+            "writer" : "로망 로랑"
+        },
+        {
+            "text" : "하루에 3시간을 걸으면 7년 후에 지구를 한바퀴 돌 수 있다.",
+            "writer" : "사무엘 존슨"
+        },
+        {
+            "text" : "언제나 현재에 집중할 수 있다면 행복할 것이다.",
+            "writer" : "파울로 코엘료"
+        },
+        {
+            "text" : "신은 용기있는 자를 결코 버리지 않는다.",
+            "writer" : "켄러"
+        },
+        {
+            "text" : "피할 수 없으면 즐겨라.",
+            "writer" : "로버트 엘리엇"
+        },
+        {
+            "text" : "우리를 향해 열린 문을 보지 못하게 된다.",
+            "writer" : "헬렌 켈러"
+        },
+        {
+            "text" : "행복은 습관이다, 그것을 몸에 지니라.",
+            "writer" : "허버드"
+        },
+        {
+            "text" : "자신감 있는 표정을 지으면 자신감이 생긴다.",
+            "writer" : "찰스 다윈"
+        },
+        {
+            "text" : "평생 살 것처럼 꿈을 꾸어라. 그리고 내일 죽을 것처럼 오늘을 살아라.",
+            "writer" : "제임스 딘"
+        },
+        {
+            "text" : "꿈을 계속 간직하고 있으면 반드시 실현할 때가 온다.",
+            "writer" : "괴테"
+        }];
         this.index = 0;
         this.main();
     }
@@ -23,6 +67,7 @@ class App {
         let now_input = "";
         let temp_input = "";
         $(".typing").on("input", function () {
+            
             if (status != true) {
                 startTime = new Date().getTime();
                 status = true;
@@ -43,10 +88,12 @@ class App {
                         status = false;
                         temp_input = "";
                         $(".now").css({ "color": "gray" });
+                        $(".red_text").css({ "color": "#666666" });
                     }
                 }, 200);
             }
             now_input = $(".typing").val();
+            let answer = texts[index].text.split("");
             if (temp_input.length <= now_input.length) {
                 count = count + 1;
                 $(".now").text("Now Speed : " + Math.round((count / ((endTime - startTime) / 1000)) * 60));
@@ -61,17 +108,17 @@ class App {
                 clearInterval(interval);
                 status = false;
                 temp_input = "";
+                $(".red_text").css({ "color": "#666666" });
             }
-            let answer = texts[index].text.split("");
-            console.log(answer);
             let my_answer = $(".typing").val().split("");
             for (let i = 0; i < my_answer.length; i++) {
                 if (answer[i] != my_answer[i]) {
-                    $(".red_text").eq(i).css({"color" : "red"});
+                    $(".red_text").eq(i).css({ "color": "red" });
                 } else {
-                    $(".red_text").css({"color" : "#666666"});
+                    $(".red_text").css({ "color": "#666666" });
                 }
             }
+            
 
             $(".typing").on("keydown", function () {
 
@@ -127,6 +174,7 @@ class App {
                     status = false;
                     temp_input = "";
                     $(".typing").val("");
+                    $(".red_text").css({ "color": "#666666" });
                 }
             });
 
